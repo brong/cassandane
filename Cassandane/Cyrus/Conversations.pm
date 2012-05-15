24e0731d4dece558737e30510427622f971928e8
Greg Banks <gnb@fastmail.fm>
2012-05-15 13:39:32 +1000

    [IRIS-1466] Test RFC2047 Subject encoding issues

--- Cassandane/Cyrus/Conversations.pm
+++ Cassandane/Cyrus/Conversations.pm
@@ -263,6 +263,9 @@ sub test_subject_variability
 	{ subject => "Message A [trailing blob]", same_cid => 0 },
 	{ subject => "Message [interpolated blob] A", same_cid => 0 },
 	{ subject => "Message A (fwd)", same_cid => 0 },    # sigh
+	{ subject => "=?utf-8?Q?Message_A?=", same_cid => 1 },
+	{ subject => "=?utf-8?Q?Message=20A?=", same_cid => 1 },
+	{ subject => "=?utf-8?Q?Mess?= =?utf-8?Q?age=20A?=", same_cid => 1 },
     );
 
     foreach my $case (@cases)
@@ -314,6 +317,10 @@ sub test_append_reply_200
 {
     my ($self) = @_;
     my %exp;
+    xlog "Test APPEND of messages related by message-id but";
+    xlog "with different subject, which form two conversations,";
+    xlog "where the subjects are identical after RFC2047 decoding";
+    xlog "but differ before it, due to vagaries in different encoders.";
 
     # check IMAP server has the XCONVERSATIONS capability
     $self->assert($self->{store}->get_client()->capability()->{xconversations});
@@ -500,6 +507,29 @@ sub test_replication_reconstruct
     $exp{"D"} = $self->make_message("Re: Message A", references => [ $exp{A} ], store => $replica_store);
     $exp{"D"}->set_attributes(uid => 22, cid => $exp{"A20"}->make_cid(), basecid => $exp{A}->make_cid());
     $self->check_messages(\%exp, keyed_on => 'uid', store => $replica_store);
+}
+
+#
+# Test append of messages with different encoding of subjects
+#
+sub test_append_subject_rfc2047
+    :min_version_3_0
+{
+    my $subject1 = "=?utf-8?Q?Dropbox_funksjonalitet_p=C3=A5_M?=\r\n=?utf-8?Q?y_Opera/Browse_Me?=";
+    my $subject2 = "=?UTF-8?Q?Re:=20Dropbox=20funksj?=\r\n=?UTF-8?Q?onalitet=20p=C3=A5=20My=20Ope?=\r\n=?UTF-8?Q?ra/Browse=20Me?=";
+
+    xlog "generating message A";
+    $exp{A} = $self->make_message($subject1);
+    $exp{A}->set_attributes(cid => $exp{A}->make_cid());
+    $self->check_messages(\%exp, keyed_on => 'uid');
+
+    xlog "generating message B";
+    $exp{B} = $self->make_message($subject2,
+				  references => [ $exp{A} ]);
+    $exp{B}->set_attributes(cid => $exp{A}->cid());
+    $self->check_messages(\%exp, keyed_on => 'uid');
+
+    $self->assert_str_equals($exp{A}->cid(), $exp{B}->cid());
 }
 
 
